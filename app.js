fbb2bf0ba44349678db27761265d608c369f9ed9
Araiz Zahid <araizzahid45@gmail.com>
2023-07-15 03:36:21 +0500

    Complete Blog Website

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+let subjectArr = [];
+let descArr = [];
+let longdescArr = [];
+
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("index", {subject: subjectArr, desc: descArr});
 })
 
 app.get("/about", function (req, res) {
@@ -15,6 +19,34 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function(req, res) {
     res.render("contact");
+})
+
+app.get("/add-post", function(req, res) {
+    res.render("addPost");
+})
+
+app.get("/blog", function(req, res) {
+    res.render("blog");
+})
+
+app.post("/add-post", function(req, res) {
+    let subject = req.body.subject;
+    let desc = req.body.desc;
+    let decsShort = desc.slice(0, 200);
+    subjectArr.push(subject);
+    descArr.push(decsShort);
+    longdescArr.push(desc)
+    // console.log(subject, desc);
+    res.redirect("/");
+})
+
+app.post("/", function(req, res){
+    console.log(req.body.btn);
+    let btn = Number(req.body.btn);
+    let subject = subjectArr[btn];
+    let desc = longdescArr[btn];
+    res.render("blog", {subject: subject, desc: desc});
+    
 })
 
 app.listen(3000, function(){
